a5285f18ef91f64ee6fa45ab0e9b0376fbaa7783
Cameron Lund <clund6314@gmail.com>
2021-08-05 14:32:31 -0600

    added credit for image

--- src/mobile/footer-mob/FooterMob.jsx
+++ src/mobile/footer-mob/FooterMob.jsx
@@ -21,6 +21,9 @@ const FooterMob = () => {
         <div className="foot-cp-mob">
           <h6>&copy; 2021 Cameron Lund</h6>
         </div>
+        <div className="foot-cp-mob">
+          <h6>Background Image by <a href="https://pixabay.com/users/manuchi-1728328/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2462430">Денис Марчук</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2462430">Pixabay</a></h6>
+        </div>
         <div className="social-mob">
           <Fade>
 
