62308ee86c4e485a7dbe3a5fe29d4d6ce69ea7c7
Cameron Lund <clund6314@gmail.com>
2021-08-05 14:26:24 -0600

    fixed an alignment error

--- src/components/shared/home/about/AboutMeCard.jsx
+++ src/components/shared/home/about/AboutMeCard.jsx
@@ -10,7 +10,7 @@ const AboutMeCard = () => (
       <p className="words-p">
         {" "}
         Hey! <br></br>
-        <br></br> I'm Cameron, a Full-Stack Web Developer hoping to break into the industry and make custom websites for clients. Currently, the main languages I use are HTML, CSS, JavaScript, Ruby, and React. I also have knowledge in C#, WordPress, PHP, and SQL. I graduated from the DevPoint Labs Full-Stack Web Developer Bootcamp in 2021 where we used Ruby on Rails and React.js to build full-stack applications.{" "}
+        <br></br> I'm Cameron, a Full-Stack Web Developer hoping to break into the industry and make custom websites for clients. Currently, the main languages I use are HTML, CSS, JavaScript, Ruby, and React. I also have knowledge in C#, WordPress, PHP, and SQL. I graduated from the DevPoint Labs Full-Stack Web Developer Bootcamp in 2021 where we used Ruby on Rails and React.js to build full-stack applications. I also attend Utah Valley University in Orem, UT, where I'm attempting to complete my bachelor's degree.{" "}
       </p>
       <p className="words-p">
         I'm actively learning new languages and trying to up my proficiency with C#, JavasScript, and React.
