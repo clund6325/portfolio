34c9797071b5b9118147925d0dc91d22876533ba
Cameron Lund <clund6314@gmail.com>
2024-02-17 15:41:21 -0700

    trying tic again

--- src/components/shared/home/about/AboutMeCard.jsx
+++ src/components/shared/home/about/AboutMeCard.jsx
@@ -10,10 +10,7 @@ const AboutMeCard = () => (
       <p className="words-p">
         {" "}
         Hey! <br></br>
-        <br></br> I'm Cameron, a Full-Stack Software Engineer. As a dedicated and passionate software engineer, I bring two years of experience working in Software Asset Management (SAM) services, where I honed my skills in a diverse array of programming languages and technologies. My expertise lies in C#, Javascript, TypeScript, AngularJS, SQL, HTML, CSS, React, and Ruby, allowing me to build robust, scalable and efficient software solutions. My journey in software engineering is driven by a deep curiosity and a persistent desire to solve complex problems, resulting in software that not only meets but exceeds expectations. Whether working on back-end processes, front-end interfaces, or full-stack applications, I approach each project with a commitment to quality and a focus on delivering tangible results. I graduated from the DevPoint Labs Full-Stack Web Developer Bootcamp in 2021 where we used Ruby on Rails and React.js to build full-stack applications. I also graduated from Utah Valley University in 2022 with a BS in Information Systems with an emphasis in App Development.{" "}
-      </p>
-      <p className="words-p">
-        I'm dedicated to continuous learning and improvement in the ever-evolving field of software engineering. I am always looking for new opportunities to grow and expand my skill set. I am passionate about creating software that is not only functional but also user-friendly and visually appealing. I am excited to bring my unique blend of technical expertise, creativity, and problem-solving skills to a team that values innovation and collaboration.{" "}
+        <br></br>I'm Cameron, a Full-Stack Software Engineer. As a software engineer with two years of experience in Software Asset Management (SAM) services, I specialize in C#, JavaScript, TypeScript, AngularJS, SQL, HTML, CSS, React, and Ruby. My expertise lies in developing robust and efficient software solutions, with a focus on both back-end and front-end technologies. I'm driven by a passion for solving complex challenges and a commitment to continuous learning, constantly striving to exceed expectations in the dynamic field of software engineering.{" "}
       </p>
     </div>
   </>
